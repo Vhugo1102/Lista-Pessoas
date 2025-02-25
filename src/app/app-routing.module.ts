a63ce50149bea4ed454e72733558a45a04e8e50c
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-02-25 15:36:37 -0300

    Corrigido problema de rotas e configurado router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListaPessoasComponent } from './components/lista-pessoas/lista-pessoas.component';
+import { FormPessoaComponent } from './components/form-pessoa/form-pessoa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListaPessoasComponent }, // Página inicial - Lista de pessoas
+  { path: 'cadastro', component: FormPessoaComponent }, // Cadastro de nova pessoa
+  { path: 'editar/:id', component: FormPessoaComponent } // Edição de pessoa
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaPessoasComponent } from './components/lista-pessoas/lista-pessoas.component';
 import { FormPessoaComponent } from './components/form-pessoa/form-pessoa.component';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
     ListaPessoasComponent,
-    FormPessoaComponent,
-
+    FormPessoaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
