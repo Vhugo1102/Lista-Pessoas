0d1602a8470969c95f44d19da107b65af48c0701
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-02-25 15:06:13 -0300

    Corrigido erro de importação do SCSS e implementada listagem de pessoas

--- src/app/components/lista-pessoas/lista-pessoas.component.ts
+++ src/app/components/lista-pessoas/lista-pessoas.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PessoaService } from 'src/app/services/pessoa.service';
+import { Pessoa } from 'src/app/models/pessoa';
 
 @Component({
   selector: 'app-lista-pessoas',
   templateUrl: './lista-pessoas.component.html',
   styleUrls: ['./lista-pessoas.component.scss']
 })
-export class ListaPessoasComponent {
+export class ListaPessoasComponent implements OnInit {
+  pessoas: Pessoa[] = [];
 
+  constructor(private pessoaService: PessoaService) {}
+
+  ngOnInit(): void {
+    this.carregarPessoas();
+  }
+
+  carregarPessoas(): void {
+    this.pessoaService.getPessoas().subscribe((dados) => {
+      this.pessoas = dados;
+    });
+  }
+
+  excluirPessoa(id: number): void {
+    if (confirm('Tem certeza que deseja excluir esta pessoa?')) {
+      this.pessoaService.deletePessoa(id).subscribe(() => {
+        this.carregarPessoas(); // Atualiza a lista após excluir
+      });
+    }
+  }
 }
